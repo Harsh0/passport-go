c98722937af247aa1ee69cb09bbb5a1b2204b49c
Harsh Singhal <harsh.singhal040@gmail.com>
2018-02-12 17:24:41 +0530

    commented test

--- test.go
+++ test.go
@@ -1,28 +1,28 @@
-package passport
+// package passport
 
-import (
-	"fmt"
-	"goodcop/passport"
+// import (
+// 	"fmt"
 
-	"github.com/gin-gonic/gin"
-)
+// 	"github.com/gin-gonic/gin"
+// 	"github.com/harsh0/passport-go"
+// )
 
-/*GithubSignupForUser allows user signup with github*/
-func GithubSignupForUser() {
-	passport.GetAuthURL("github", encrypted)
-}
+// /*GithubSignupForUser allows user signup with github*/
+// func GithubSignupForUser() {
+// 	passport.GetAuthURL("github", encrypted)
+// }
 
-/*GoogleSignupForUser allows user signup with google*/
-func GoogleSignupForUser() {
-	url, err := passport.GetAuthURL("google", encrypted)
-}
+// /*GoogleSignupForUser allows user signup with google*/
+// func GoogleSignupForUser() {
+// 	url, err := passport.GetAuthURL("google", encrypted)
+// }
 
-/*TwitterSignupForUser allows user signup with twitter*/
-func TwitterSignupForUser(c *gin.Context) {
-	fmt.Println("Twitter signup")
-}
+// /*TwitterSignupForUser allows user signup with twitter*/
+// func TwitterSignupForUser(c *gin.Context) {
+// 	fmt.Println("Twitter signup")
+// }
 
-/*GithubCallback allows user signup with github*/
-func GithubCallback() {
-	// profile, err := passport.Authenticate("github", code, state)
-}
+// /*GithubCallback allows user signup with github*/
+// func GithubCallback() {
+// 	// profile, err := passport.Authenticate("github", code, state)
+// }
